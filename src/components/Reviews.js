cf301ea7a40afa80a5acc13915b7648ac7e56584
styper <stypertypes@gmail.com>
2022-07-19 12:43:40 +0530

    Reviews section with button click to see all reviews one by one

--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
+import ReviewsList from './ReviewsList';
 
-function Reviews(){
+function Reviews() {
+    
+    const [rev, setRev] = useState(0);
+
+    function dispRev(r){
+        if(r.id === rev){
+            return (r.content);
+        }
+    }
+
+    function handleClick(){
+        if(rev === 3){
+            setRev(0);
+        }
+        else{
+            setRev(rev + 1);
+        }
+    }
+
     return (
-        <div id = "reviews">
-            <div className = "reviewsBlocks">
-                <div className = "revLeft">
-                    <h1>This is what our Customers Say</h1>
+        <div id="reviews">
+            <div className="reviewsBlocks">
+                <div className="revLeft">
+                    <h1>This is what Our Customers Say</h1>
+                    <div className = "reviewBox">
+                        <h3>Name</h3>
+                        <p>{ReviewsList.map(dispRev)}</p>
+                        <button onClick={handleClick}>Next</button>
+                    </div>
                 </div>
-                <div className = "revRight"></div>
+                <div className="revRight"></div>
             </div>
         </div>
     );
---
+++ src/components/ReviewsList.js
@@ -0,0 +1,20 @@
+const reviewsList = [
+    {
+        id: 0,
+        content: "This is the 1st review"
+    },
+    {
+        id: 1,
+        content: "This is the 2nd review"
+    },
+    {
+        id: 2,
+        content: "This is the 3rd review"
+    },
+    {
+        id: 3,
+        content: "This is the 4th review"
+    },
+];
+
+export default reviewsList;
