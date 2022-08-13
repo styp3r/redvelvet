9e9acd3579b5dfe025a5265db2b3e4b1c9f5aa25
styper <stypertypes@gmail.com>
2022-08-13 18:20:54 +0530

    logo resize

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,13 +2,14 @@ import React from 'react';
 import Fb from '../images/Decoration/fbIcon.png';
 import In from '../images/Decoration/inIcon.png';
 import Wa from '../images/Decoration/waIcon.png';
+import LogoFooter from '../images/Decoration/logoFooter.jpg';
 
 function Footer() {
     return (
         <div id="footerSection">
                 <div className="footerContent">
                     <div className="footerLeft">
-                        <p className="footerLogo">Laddoos</p>
+                        <img className = "footerLogo" src = {LogoFooter}/>
                         <p className = "copyright">Copyright &#xA9; 2022 Xyka Hotels India Pvt. Ltd. | All rights reserved</p>
                     </div>
                     <div className="footerRight">
