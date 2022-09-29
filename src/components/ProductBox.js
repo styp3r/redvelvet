a7c5b6cae95ff5717f805ef3eda0e91ce11cb303
styp3r <stypertypes.com>
2022-09-29 20:29:01 +0530

    Setting size to lazyload component as images are not displaying correctly- products page

--- src/components/ProductBox.js
+++ src/components/ProductBox.js
@@ -1,15 +1,17 @@
+import LazyLoad from 'react-lazy-load';
+
 function ProductBox(props) {
     if (props.category === props.current) {
         return (
             <div id="productBox">
-                <img className="itemBoxImg" src={props.img} loading = "lazy" />
+                <LazyLoad width="100%" height="60%"><img className="itemBoxImg" src={props.img} /></LazyLoad>
                 <div className="itemBoxText">
                     <p className="itemBoxName">{props.name}</p>
                     <p className="itemBoxQty">{props.qty}</p>
                 </div>
             </div>
         );
-    } 
+    }
 }
 
 export default ProductBox;
