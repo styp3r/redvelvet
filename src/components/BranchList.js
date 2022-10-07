2035309d6ed2713d74ce415cd576ec79db52c042
styp3r <stypertypes.com>
2022-10-07 19:09:25 +0530

    Adds shop images for branches

--- src/components/BranchList.js
+++ src/components/BranchList.js
@@ -1,7 +1,17 @@
+import JBN from '../images/Shops/JBN.jpg';
+import BTM from '../images/Shops/BTM.jpg';
+import DDK from '../images/Shops/DDK.jpeg';
+import EC2 from '../images/Shops/EC2.jpeg';
+import HMV from '../images/Shops/HMV.jpeg';
+import KNR from '../images/Shops/KNR.jpeg';
+import MLL from '../images/Shops/MLL.jpeg';
+import ORR from '../images/Shops/ORR.jpeg';
+import VTR from '../images/Shops/VTR.jpg';
+
 const branchList = [
     {
         id: 0,
-        img: "https://jkfenner.com/wp-content/uploads/2019/11/default.jpg",
+        img: JBN,
         name: "Jeevan Bima Nagar",
         address: "No. 9, Jeevan Bima Nagar Main Rd, NAL Colony, Jeevan Bima Nagar, Bengaluru, Karnataka 560075",
         mapLink: "https://www.google.com/maps/place/Laddoos/@12.9677487,77.650868,17z/data=!3m1!4b1!4m5!3m4!1s0x3bae1400773f82b3:0x8a90bb8f01d7d48a!8m2!3d12.9677659!4d77.653064",
@@ -9,15 +19,15 @@ const branchList = [
     },
     {
         id: 1,
-        img: "https://jkfenner.com/wp-content/uploads/2019/11/default.jpg",
-        name: "BTM",
-        address: "No 951, 16th Main Rd, BTM 2nd Stage, MCHS Colony, Stage 2, BTM Layout, Bengaluru, Karnataka 560076",
-        mapLink: "https://www.google.com/maps/place/LADDOOS/@12.9136472,77.6079758,17z/data=!3m1!4b1!4m5!3m4!1s0x3bae14fd062570bf:0xe1a28beb800dc960!8m2!3d12.9136469!4d77.610179",
-        phone: "+91 63636 21722",
+        img: KNR,
+        name: "Kasturi Nagar",
+        address: "305/J, 2nd Main Rd, opp. Karnataka Bank, East of NGEF Layout, Kasturi Nagar, Bengaluru, Karnataka 560043",
+        mapLink: "https://www.google.com/maps/place/Laddoos/@13.0048975,77.6579527,17z/data=!3m1!4b1!4m5!3m4!1s0x3bae1162bf74b56b:0xf3b64d93ab5ac57!8m2!3d13.0048954!4d77.660142",
+        phone: "+91 96320 01866",
     },
     {
         id: 2,
-        img: "https://jkfenner.com/wp-content/uploads/2019/11/default.jpg",
+        img: VTR,
         name: "Varthur",
         address: "No. 224/4, Varthur Main Road, Next to Shell Petrol Pump, Gunjuru Village, Varthur, Hobli, Bengaluru, Karnataka, 560087",
         mapLink: "https://www.google.com/maps/place/Laddoos/@12.9321994,77.7403157,17z/data=!3m1!4b1!4m5!3m4!1s0x3bae13d457d1d96b:0x4fee9be8c87a017f!8m2!3d12.9321994!4d77.7425098",
@@ -25,7 +35,7 @@ const branchList = [
     },
     {
         id: 3,
-        img: "https://jkfenner.com/wp-content/uploads/2019/11/default.jpg",
+        img: EC2,
         name: "Electronic City",
         address: "Opp. SJR Equinox, TOWER-A2, Velankani Drive, Electronics City Phase 1, Electronic City, Bengaluru, Karnataka 560100",
         mapLink: "https://www.google.com/maps/place/Laddoos/@12.8488613,77.653063,17z/data=!4m5!3m4!1s0x3bae6c9cf5f23bcd:0xb8d1875ab5328d1f!8m2!3d12.8488648!4d77.6575993",
@@ -33,7 +43,7 @@ const branchList = [
     },
     {
         id: 4,
-        img: "https://jkfenner.com/wp-content/uploads/2019/11/default.jpg",
+        img: ORR,
         name: "Bellandur",
         address: "No. 78, 5, Bellandur Main Rd, opposite Ganesha Temple, Bellandur, Bengaluru, Karnataka 560103",
         mapLink: "https://www.google.com/maps/place/Laddoos/@12.9280674,77.6741691,17z/data=!3m1!4b1!4m5!3m4!1s0x3bae139f899a8d6f:0x1669fb811a8cf5e!8m2!3d12.9280654!4d77.6764101",
@@ -41,7 +51,7 @@ const branchList = [
     },
     {
         id: 5,
-        img: "https://jkfenner.com/wp-content/uploads/2019/11/default.jpg",
+        img: DDK,
         name: "Doddanekundi",
         address: "11th Cross opp SJR apartments, Kundalahalli Colony, Bengaluru, Karnataka 560037",
         mapLink: "https://www.google.com/maps/place/Laddoos/@12.9717059,77.7117268,15z/data=!4m2!3m1!1s0x0:0x585a3b074a6698ba?sa=X&ved=2ahUKEwjAsfaO6c36AhWSTmwGHRY7BJgQ_BJ6BQiZARAF",
@@ -49,7 +59,7 @@ const branchList = [
     },
     {
         id: 6,
-        img: "https://jkfenner.com/wp-content/uploads/2019/11/default.jpg",
+        img: MLL,
         name: "Munnekollal",
         address: "Green Garden Layout, Silver Springs Layout, Munnekollal, Bengaluru, Karnataka 560066",
         mapLink: "https://www.google.com/maps/place/Laddoos/@12.9513172,77.7133876,17z/data=!3m1!4b1!4m5!3m4!1s0x3bae13c75c1a72bf:0x88a490319ac46b6f!8m2!3d12.951313!4d77.7155404",
@@ -57,7 +67,7 @@ const branchList = [
     },
     {
         id: 7,
-        img: "https://jkfenner.com/wp-content/uploads/2019/11/default.jpg",
+        img: HMV,
         name: "Horamavu",
         address: "Kalkere Main Rd, Dodda Kempaiah Layout, Kalkere, Bengaluru, Karnataka 560016",
         mapLink: "https://www.google.com/maps/place/Laddoos/@13.0314441,77.6694778,15z/data=!4m5!3m4!1s0x0:0x681e5c5bb703e58b!8m2!3d13.0314791!4d77.6694829",
@@ -65,11 +75,11 @@ const branchList = [
     },
     {
         id: 8,
-        img: "https://jkfenner.com/wp-content/uploads/2019/11/default.jpg",
-        name: "Kasturi Nagar",
-        address: "305/J, 2nd Main Rd, opp. Karnataka Bank, East of NGEF Layout, Kasturi Nagar, Bengaluru, Karnataka 560043",
-        mapLink: "https://www.google.com/maps/place/Laddoos/@13.0048975,77.6579527,17z/data=!3m1!4b1!4m5!3m4!1s0x3bae1162bf74b56b:0xf3b64d93ab5ac57!8m2!3d13.0048954!4d77.660142",
-        phone: "+91 96320 01866",
+        img: BTM,
+        name: "BTM",
+        address: "No 951, 16th Main Rd, BTM 2nd Stage, MCHS Colony, Stage 2, BTM Layout, Bengaluru, Karnataka 560076",
+        mapLink: "https://www.google.com/maps/place/LADDOOS/@12.9136472,77.6079758,17z/data=!3m1!4b1!4m5!3m4!1s0x3bae14fd062570bf:0xe1a28beb800dc960!8m2!3d12.9136469!4d77.610179",
+        phone: "+91 63636 21722",
     },
 ];
 
