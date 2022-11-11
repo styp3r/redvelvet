56addc025510f12e3c42ef30a546c0722dfa1015
styp3r <stypertypes.com>
2022-11-11 18:02:19 +0530

    Social link for facebook added for mobile

--- src/App.js
+++ src/App.js
@@ -26,25 +26,25 @@ function App() {
   }
 
   return (
-    <div id = "app-overlay">
+    <div id="app-overlay">
       <div id="navDrawer">
         <div className="closeDrawerBtn" onClick={handleCloseBurger}>
-          <span id = "closeBtn" className="material-symbols-outlined">close</span>
+          <span id="closeBtn" className="material-symbols-outlined">close</span>
         </div>
         <div className="drawerLinks">
-          <a aria-label = "redirects user to home page" href={'/'}><p className="dLink">Home</p></a>
-          <a aria-label = "redirects user to products page" href={'/products'}><p className="dLink">Menu</p></a>
-          <a aria-label = "redirects user to about page" href={'/about'}><p className="dLink">About Us</p></a>
-          <a aria-label = "redirects user to branches page" href={'/branches'}><p className="dLink">Branches</p></a>
-          <a aria-label = "redirects user to contact page" href={'/contact'}><p className="dLink">Contact Us</p></a>
+          <a aria-label="redirects user to home page" href={'/'}><p className="dLink">Home</p></a>
+          <a aria-label="redirects user to products page" href={'/products'}><p className="dLink">Menu</p></a>
+          <a aria-label="redirects user to about page" href={'/about'}><p className="dLink">About Us</p></a>
+          <a aria-label="redirects user to branches page" href={'/branches'}><p className="dLink">Branches</p></a>
+          <a aria-label="redirects user to contact page" href={'/contact'}><p className="dLink">Contact Us</p></a>
 
           <div className="social">
-            <img alt = "loading..." className="fb" src={Fb} />
-            <a target = "_blank" rel="noreferrer" href="https://www.instagram.com/laddoossweetsindia/"><img alt = "loading..." className="in" src={In} /></a>
-            <a target = "_blank" rel="noreferrer" href = "https://wa.me/+919945258005"><img alt = "loading..." className="wa" src={Wa} /></a>
+            <a aria-label="Redirects user to our Facebook profile" rel="noreferrer" target="_blank" href="https://www.facebook.com/profile.php?id=100072360552388"><img alt="loading..." className="fb" src={Fb} /></a>
+            <a aria-label="Redirects user to our Instagram profile" target="_blank" rel="noreferrer" href="https://www.instagram.com/laddoossweetsindia/"><img alt="loading..." className="in" src={In} /></a>
+            <a aria-label="Redirects user to our WhatsApp Chat" target="_blank" rel="noreferrer" href="https://wa.me/+919945258005"><img alt="loading..." className="wa" src={Wa} /></a>
           </div>
           <div>
-          <p className="footerTag">Healthy, Fast-Casual Food, Crafted with &#x2764; In Bengaluru</p>
+            <p className="footerTag">Healthy, Fast-Casual Food, Crafted with &#x2764; In Bengaluru</p>
           </div>
         </div>
 
